aa9eadfdb36f4b712ba2d9ca28a4211592caae53
Shannon Gatta <shannon.gatta@gmail.com>
2018-01-17 21:13:15 -0800

    1/17

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,20 +3,22 @@
 ## the game is called 'is it going to rain in Seattle?'
 
 ## make a variable called 'chance_of_rain' and assign it to be 0
-
+chance_of_rain <- 0
 ## assign 'true' or 'false' to a variable named 'I_saw_raindrops'
-
+I_saw_raindrops <- TRUE
 ## assign 'true' or 'false' to a variable named 'my_shoes_are_wet'
-
+my_shoes_are_wet <- FALSE
 ## assign 'true' or 'false' to a variable named 'my_ta_is_soaked'
-
+my_ta_is_soaked <- FALSE
 ## assign 'true' or 'false' to a variable named 'I_love_cookies'
-
+I_love_cookies <- TRUE
 ## ok, now you have your variables ready to go.
 
 ## make an if statement that checks whether you saw raindrops
 ## if you did, set chance_of_rain to 100
-
+if (I_saw_raindrops = TRUE) {
+  chance_of_rain <- 100
+  }
 ## else make an if statement that checks whether your shoes are wet
 ## if they are, chance of rain goes up by 40 percent
 
